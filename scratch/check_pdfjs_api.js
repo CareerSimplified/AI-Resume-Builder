1737b77eab77eee373f310931e32d1ee132c26ef
harshankgit <harshankgit@gmail.com>
2026-04-17 23:21:55 +0530

    Cleanup: Remove scratch test files and legacy pdf-parse references to prevent Vercel build confusion

--- scratch/check_pdfjs_api.js
+++
@@ -1,7 +0,0 @@
-const pdfjs = require('pdfjs-dist');
-console.log('PDFJS keys:', Object.keys(pdfjs));
-if (pdfjs.getDocument) {
-    console.log('getDocument found');
-} else {
-    console.log('getDocument NOT found');
-}
